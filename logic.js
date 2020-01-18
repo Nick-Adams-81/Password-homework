d9bb272a49cef7c12036a4f16edcaa3538dd4d5e
nick adams <thebigdeal916@icloud.com>
2020-01-18 09:38:09 -0800

    password v1

---
+++ logic.js
@@ -0,0 +1,36 @@
+var char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var num = '1234567890'
+var sym = '!@#$%^&*()_+'
+
+var charNum = document.querySelector('#charNum')
+var numBox = document.querySelector('#num')
+var symBox = document.querySelector('#sym')
+var submit = document.querySelector('#Submit')
+var yourPw = document.querySelector('#yourPw')
+var copyPw = document.querySelector('#copyPw')
+
+
+
+
+submit.addEventListener('click', function(){
+    var characters = char;
+    (numBox.checked) ? characters += num : '';
+    (symBox.checked) ? characters += sym : '';
+
+    yourPw.value = password(charNum.value, characters)
+})
+
+
+
+function password(l, characters){
+    var pwd = ''
+    for( var i = 0; i < l; i++){
+        pwd += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    return pwd;
+}
+function copyPassword(){
+    document.getElementById('copyPw').select();
+    document.execCommand('copy')
+    alert('Password copied to clipboard')
+}
